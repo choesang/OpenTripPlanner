dc7a714e48e449bc9bf919c473c9500ab0c144e6
Gard Mellemstrand <gard.mellemstrand@soprasteria.com>
2017-11-14 12:19:03 +0100

    Fallback to earlier version of stop if quay not found

--- src/main/java/org/opentripplanner/netex/mapping/NetexMapper.java
+++ src/main/java/org/opentripplanner/netex/mapping/NetexMapper.java
@@ -26,7 +26,6 @@ public class NetexMapper {
 
     TripPatternMapper tripPatternMapper = new TripPatternMapper();
 
-    CalendarMapper calendarMapper = new CalendarMapper();
 
     public NetexMapper(OtpTransitDaoBuilder transitBuilder) {
         this.transitBuilder = transitBuilder;
@@ -46,9 +45,10 @@ public class NetexMapper {
             }
         }
 
-        for (StopPlace stopPlace : netexDao.getStopPlaceMap().values()) {
-            if (stopPlace != null) {
-                Collection<Stop> stops = stopMapper.mapParentAndChildStops(stopPlace, netexDao.getParentStopPlaceById());
+        for (String stopPlaceId : netexDao.getStopsById().keySet()) {
+            Collection<StopPlace> stopPlaceAllVersions = netexDao.getStopsById().get(stopPlaceId);
+            if (stopPlaceAllVersions != null) {
+                Collection<Stop> stops = stopMapper.mapParentAndChildStops(stopPlaceAllVersions, netexDao.getPare.);
                 for (Stop stop : stops) {
                     transitBuilder.getStops().add(stop);
                 }
@@ -58,6 +58,8 @@ public class NetexMapper {
         for (JourneyPattern journeyPattern : netexDao.getJourneyPatternsById().values()) {
             if (journeyPattern != null) {
                 tripPatternMapper.mapTripPattern(journeyPattern, transitBuilder, netexDao);
+            } else {
+                int i = 0;
             }
         }
 
