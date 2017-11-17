02d6a6a5f8da9e27ee0e1b83c9bad74fd5cc6a4f
Gard Mellemstrand <gard.mellemstrand@soprasteria.com>
2017-11-17 13:14:08 +0100

    Removed debugging code

--- src/main/java/org/opentripplanner/netex/mapping/NetexMapper.java
+++ src/main/java/org/opentripplanner/netex/mapping/NetexMapper.java
@@ -63,8 +63,6 @@ public class NetexMapper {
         for (JourneyPattern journeyPattern : netexDao.getJourneyPatternsById().values()) {
             if (journeyPattern != null) {
                 tripPatternMapper.mapTripPattern(journeyPattern, transitBuilder, netexDao);
-            } else {
-                int i = 0;
             }
         }
 
