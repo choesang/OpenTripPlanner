6582439177bd264baaeddddfaec769431aaa691c
Gard Mellemstrand <gard.mellemstrand@soprasteria.com>
2017-11-21 11:36:09 +0100

    Fixed failing test StopTimeMapper (GTFS)

--- src/test/java/org/opentripplanner/gtfs/mapping/StopTimeMapperTest.java
+++ src/test/java/org/opentripplanner/gtfs/mapping/StopTimeMapperTest.java
@@ -93,7 +93,7 @@ public class StopTimeMapperTest {
     public void testMap() throws Exception {
         org.opentripplanner.model.StopTime result = subject.map(STOP_TIME);
 
-        assertEquals(ID, result.getId());
+        assertEquals(ID, (Integer) Integer.parseInt(result.getId().getId()));
         assertEquals(ARRIVAL_TIME, result.getArrivalTime());
         assertEquals(DEPARTURE_TIME, result.getDepartureTime());
         assertEquals(DROP_OFF_TYPE, result.getDropOffType());
