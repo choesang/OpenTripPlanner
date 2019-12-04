d34fac849b9a1e54f1cfcb57a3b3082b50a56954
Gard Mellemstrand <gard.mellemstrand@entur.org>
2019-12-04 11:53:42 +0100

    Set sandbox features to false, as these are set via otp-config.json

--- src/main/java/org/opentripplanner/util/OTPFeature.java
+++ src/main/java/org/opentripplanner/util/OTPFeature.java
@@ -23,8 +23,8 @@ public enum OTPFeature {
     APIUpdaterStatus(true),
 
     // Sandbox extension features - Must be turned OFF by default
-    ActuatorAPI(true),
-    SandboxAPITransmodelApi(true),
+    ActuatorAPI(false),
+    SandboxAPITransmodelApi(false),
     SandboxExampleAPIGraphStatistics(false),
     TransferAnalyzer(false);
 
