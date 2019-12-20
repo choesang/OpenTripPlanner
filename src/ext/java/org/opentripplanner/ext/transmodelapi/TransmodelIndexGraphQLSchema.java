1ff21e15068c8272bad8dd949a0d56d50097500c
Gard Mellemstrand <gard.mellemstrand@entur.org>
2019-12-20 12:42:12 +0100

    Fix quay->situations in transmodel api

--- src/ext/java/org/opentripplanner/ext/transmodelapi/TransmodelIndexGraphQLSchema.java
+++ src/ext/java/org/opentripplanner/ext/transmodelapi/TransmodelIndexGraphQLSchema.java
@@ -1733,7 +1733,7 @@ public class TransmodelIndexGraphQLSchema {
                         .description("Get all situations active for the quay.")
                         .type(new GraphQLNonNull(new GraphQLList(ptSituationElementType)))
                     .dataFetcher(dataFetchingEnvironment -> graph.getSiriAlertPatchService()
-                        .getStopPatches(dataFetchingEnvironment.getSource()))
+                        .getStopPatches(((Stop) dataFetchingEnvironment.getSource()).getId()))
                         .build())
                 /*
                 .field(GraphQLFieldDefinition.newFieldDefinition()
